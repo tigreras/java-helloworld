abd86060643d8ab00b44aa16162e2713a4f31e74
Pieter Daud Satumalay <peter.crusties@gmail.com>
2021-03-19 11:13:42 +0700

    Update Server.java

--- src/main/java/ch/vorburger/openshift/s2i/example/Server.java
+++ src/main/java/ch/vorburger/openshift/s2i/example/Server.java
@@ -48,7 +48,7 @@ public class Server implements AutoCloseable {
         int port = 8080;
         httpServer = HttpServer.create(new InetSocketAddress(port), 0);
         httpServer.createContext("/", exchange -> {
-            String response = "<center><h1>Cloud Admin - Java V1</h1><p><h3>Hello World ITX!</h3></p></center>";
+            String response = "<center><h1>Cloud Admin - Java v1</h1><p><h3>Hello World ITX!</h3></p></center>";
             exchange.sendResponseHeaders(HTTP_OK_STATUS, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
